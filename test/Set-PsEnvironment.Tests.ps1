07d7db28099c99157672069f8050198fd261e0ed
Tom-Inge Larsen <tom-inge.larsen@advania.no>
2017-09-10 20:55:55 +0200

    Pester tests for both functions

--- test/Set-PsEnvironment.Tests.ps1
+++ test/Set-PsEnvironment.Tests.ps1
@@ -41,10 +41,56 @@ Describe 'Create environment config file tests' {
     }
 }
 
-Describe "Configure environment tests" {
-    $config = New-PsEnvironmentConfig -InstallGit -GitUserName "Your Name" -GitEmail "your@email.com" -InstallVscode -AdditionalVsCodeExtensions 'eamodio.gitlens' -PsModules 'pester','plaster' -PsProfile $profile -IncludeTests
+Describe "Configure environment tests -- All params configured" {
+    $config = New-PsEnvironmentConfig -InstallGit -GitUserName "Your Name" -GitEmail "your@email.com" -InstallVscode -AdditionalVsCodeExtensions 'eamodio.gitlens' -PsModules 'pester', 'plaster' -PsProfile $profile -IncludeTests
+    #Get current git config, Set-PsEnvironment will change user.name and user.email, need to revert after test has run.
+    #Can't find a way to mock it.
+    $gitc = git config --list | Sort-Object -Unique
+    $gitconfig = @{}
 
-    Mock Install-Git.ps1 {}
-    Mock Install-VSCode.ps1 {} -ParameterFilter {$AdditionalExtensions -eq $config.AdditionalVsCodeExtensions}
-    mock git {}
+    #Convert to hashtable, git config is on the format item.config=Item value
+    foreach ($item in $gitc) {
+        $hashitem = $item -split '='
+        $gitconfig.Add($hashitem[0], $hashitem[1])
+    }
+
+    Mock Install-Git.ps1 -ModuleName Set-PsEnvironment
+    Mock Install-Module -ParameterFilter {$name -eq "posh-git"} -ModuleName Set-PsEnvironment
+    Mock Install-VSCode.ps1 -ParameterFilter {$AdditionalExtensions -eq $config.AdditionalVsCodeExtensions} -ModuleName Set-PsEnvironment
+    Mock Install-VSCode.ps1 -ModuleName Set-PsEnvironment
+    Mock Install-Module -ModuleName Set-PsEnvironment
+    Mock New-Item -ModuleName Set-PsEnvironment
+    Mock Set-Content -ParameterFilter {$path -and $path.EndsWith("profile.tests.ps1") } -ModuleName Set-PsEnvironment
+    Mock Set-Content -ParameterFilter {$path -and $path -eq $profile } -ModuleName Set-PsEnvironment
+    Mock Set-Content -ModuleName Set-PsEnvironment
+
+    Set-PsEnvironment -Config $config
+
+    It "Calls Install-Vscode" {
+        Assert-MockCalled Install-VSCode.ps1 -ParameterFilter {$AdditionalExtensions -eq $config.AdditionalVsCodeExtensions} -Exactly 1 -ModuleName Set-PsEnvironment
+    }
+
+    It "Calls Install-Git" {
+        Assert-MockCalled Install-Git.ps1 -Exactly 1 -ModuleName Set-PsEnvironment
+    }
+
+    It "Installs Posh-Git" {
+        Assert-MockCalled Install-Module -ParameterFilter {$name -eq "posh-git"} -ModuleName Set-PsEnvironment -Exactly 1
+    }
+
+    It "Calls Install-Module" {
+        Assert-MockCalled Install-Module -Exactly 3 -ModuleName Set-PsEnvironment
+    }
+
+    It "Adds Profile Tests" {
+        Assert-MockCalled Set-Content -ParameterFilter {$path -and $path.EndsWith("profile.tests.ps1") } -Exactly 1 -ModuleName Set-PsEnvironment
+    }
+
+    It "Adds Profile content" {
+        Assert-MockCalled Set-Content -ParameterFilter {$path -and $path -eq $profile } -Exactly 1 -ModuleName Set-PsEnvironment
+    }
+
+    #Clean up git config
+    git config --global user.name $gitconfig.'user.name'
+    git config --global user.email $gitconfig.'user.email'
 }
