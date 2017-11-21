f62cac695137f5c7b0430032ceb681425ac52f9c
Tom-Inge Larsen <tom-inge.larsen@advania.no>
2017-11-21 12:56:50 +0100

    added agent specific module/script installs

--- .build/build.ps1
+++ .build/build.ps1
@@ -1,9 +1,14 @@
 # Grab nuget bits, install modules, set build variables, start build.
 Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
 
-Install-Module Psake -force
+#Install required modules
+Install-Module psake -Scope CurrentUser -Force
+Install-Module PSScriptAnalyzer -Scope CurrentUser -Force
+Install-Script Install-Git -Scope CurrentUser -Force
+Install-Module posh-git -Scope CurrentUser -Force
+Install-Script Install-VSCode -Scope CurrentUser -Force
 Install-Module Pester -Force -SkipPublisherCheck
-Import-Module Psake, Pester
+Import-Module Psake, Pester, PSScriptAnalyzer
 
 Invoke-psake .build\psake.ps1
 
