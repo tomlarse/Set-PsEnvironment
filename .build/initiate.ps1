18fcebe2da858750d315db7ef86f9d6faf3385f9
Tom-Inge Larsen <tom-inge.larsen@advania.no>
2017-11-21 12:27:36 +0100

    Profile creation for the unit tests

--- .build/initiate.ps1
+++ .build/initiate.ps1
@@ -2,4 +2,8 @@ Install-Module psake -Scope CurrentUser -Force
 Install-Module PSScriptAnalyzer -Scope CurrentUser -Force
 Import-Module psake, PSScriptAnalyzer
 
+New-Item -ItemType Directory -Force -Path (Split-Path $profile)
+
+Set-Content -Path $profile -Value "Testprofile"
+
 Invoke-PSake $PSScriptRoot\Build.ps1
