b4be5fd237817978c793aa41d504eaa13d770a9b
Tom-Inge Larsen <tom-inge.larsen@advania.no>
2017-11-21 12:39:14 +0100

    upload testresults to appveyor

--- .build/psake.ps1
+++ .build/psake.ps1
@@ -4,7 +4,13 @@ Task Test -Depends Init, Clean {
     $profile = "$($ENV:BHProjectPath)\profile.ps1"
     New-Item -ItemType File -Path $profile
     Set-Content -Path $profile -Value "profile"
-    Invoke-Pester ..\
+    $testresultsfile = .\Testresults.xml
+    $res = Invoke-Pester ..\ -OutputFormat NUnitXml -OutputFile $testresultsfile -PassThru
+
+    (New-Object 'System.Net.WebClient').UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path $testResultsFile))
+        if ($res.FailedCount -gt 0) {
+            throw "$($res.FailedCount) tests failed."
+        }
 }
 
 Task Clean -Depends Init {
