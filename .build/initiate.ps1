37c3b799c6279fb3e18dac99580bc9a425bf1de0
Tom-Inge Larsen <tom-inge.larsen@advania.no>
2017-11-21 12:54:07 +0100

    added agent specific script/module installs

--- .build/initiate.ps1
+++ .build/initiate.ps1
@@ -1,6 +1,10 @@
 #Install required modules
 Install-Module psake -Scope CurrentUser -Force
 Install-Module PSScriptAnalyzer -Scope CurrentUser -Force
+Install-Script Install-Git -Scope CurrentUser -Force
+Install-Module posh-git -Scope CurrentUser -Force
+Install-Script Install-VSCode -Scope CurrentUser -Force
+
 Import-Module psake, PSScriptAnalyzer
 
 #Create $profile location on agent.
